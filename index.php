580ff21f7ba8068c1f40c75e0c126f09134c242e
Nicolas Hiroki Yonekawa <69517285+nicolas2602@users.noreply.github.com>
2023-04-18 21:04:29 -0300

    Commitando o Sistema Aeroporto parte 2

--- index.php
+++ index.php
@@ -33,7 +33,23 @@
     </div>
 
     <?php 
-        include("include/mostrar.php"); 
+        //include("include/mostrar.php");
+        if(isset($_POST['showPais'])){
+            include("table_pais.php");
+        }else if(isset($_POST['showCidade'])){
+            include("table_cidade.php");
+        }
+        else{
+            echo("
+                <div class='d-flex justify-content-center'>
+                    <div class='container'>
+                        <div class='alert alert-warning alert-dismissible fade show text-center' role='alert'>
+                            Bem-vindo ao sistema de aeroporto! Para olhar as tabelas, clique em um dos botões acima!
+                        </div>
+                    </div>
+                </div>
+            ");
+        }
     ?>
 
     <div class="d-flex justify-content-center">
